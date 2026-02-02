a35b7d6d1214d07b6803695d1366e6194038ec97
Selim Nouira <selim.nouira@limayrac.fr>
2026-02-03 00:57:32 +0100

    test

---
+++ express-js/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, './navbar-app/index.html'));
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>page not found</h1>');
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000');
+    });
+
+
+
