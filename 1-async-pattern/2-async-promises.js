bdcbab1936ce2f4fcf954f5a9a298e1eab96b2b1
Selim Nouira <selim.nouira@limayrac.fr>
2026-02-03 00:57:17 +0100

    test

---
+++ 1-async-pattern/2-async-promises.js
@@ -0,0 +1,37 @@
+const { readFile,writeFile } = require('fs').promises
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+
+const start = async() => {
+    try {
+        const first = await readFile('./content/text.txt', 'utf8')
+        const second = await readFile('./content/text2.txt', 'utf8')
+        await writeFile('./content/result-mind-grenade.txt', `This is awesome: ${first} ${second}`, { flag: 'a' })
+        console.log(first, second)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+start();
+
+// getText('./content/text.txt')
+// .then(result => console.log(result))
+// .catch(err => console.log(err))
+
+// const getText = (path) => {
+//  return new Promise((resolve, reject) => {
+// readFile(path, 'utf8', (err, data) => {
+//     if (err) {
+//     reject(err);
+//     }
+//     else{ 
+//     resolve(data);
+//     }
+
+//         })
+//     })
+// }
